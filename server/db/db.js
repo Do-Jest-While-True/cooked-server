cfbe72b6c75bd3e0f520bdf508f2f74b66fc2bc7
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-09 16:52:05 -0400

    init

---
+++ server/db/db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const dbName = 'cooked'
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
+
+const db = new Sequelize(dbUrl, {
+	logging: false,
+	operatorAliases: false
+});
+
+module.exports = db;
