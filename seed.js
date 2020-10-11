f52b2e013333c95ae769af24cd79f24dfda27819
Bryan <bryanryu1@gmail.com>
2020-10-11 14:51:32 -0400

    faker creates comments and attaches user to comments and comment to a random recipe

--- seed.js
+++ seed.js
@@ -40,17 +40,17 @@ const seed = async () => {
     })
 
     for (let i = 1; i < 100; i++) {
-      const user = await User.findByPk(
+      const user1 = await User.findByPk(
         Math.floor(Math.random() * Math.floor(100)) + 1
       )
       const user2 = await User.findByPk(
         Math.floor(Math.random() * Math.floor(100)) + 1
       )
-      await user.setFollower(user2)
+      await user2.setFollower(user1)
     }
 
-    await user1.setFollower(user2)
-    await user2.setFollower(user1)
+    // await user1.setFollower(user2)
+    // await user2.setFollower(user1)
 
     // RECIPES / COMMENTS ____________________
     await Promise.all([...stockRecipes.map((recipe) => Recipe.create(recipe))])
@@ -63,23 +63,37 @@ const seed = async () => {
 
     let comments = []
     for (let i = 0; i < 50; i++) {
-      let comment = {
+      let newComment = {
         body: faker.lorem.sentence(),
       }
-      comments.push(comment)
+      comments.push(newComment)
     }
 
     comments.forEach(async (comment) => {
       await Comment.create(comment)
     })
 
-    for (let i = 1; i < 50; ) await recipe.addComment(comment)
+    for (let i = 1; i < 50; i++) {
+      const user = await User.findByPk(
+        Math.floor(Math.random() * Math.floor(100)) + 1
+      )
+      const recipeId = await Recipe.findByPk(
+        Math.floor(Math.random() * Math.floor(4)) + 1
+      )
+      const comment = await Comment.findByPk(
+        Math.floor(Math.random() * Math.floor(48)) + 1
+      )
+      await comment.setUser(user)
+      await recipeId.addComment(comment)
+    }
+
+    await recipe.addComment(comment1)
     await comment1.setUser(user2)
 
     // MAGIC METHODS ________________________
     console.log('user', Object.keys(user1.__proto__))
     console.log('recipe', Object.keys(recipe.__proto__))
-    console.log('comment', Object.keys(comment.__proto__))
+    console.log('comment', Object.keys(comment1.__proto__))
   } catch (err) {
     console.log(red(err))
   }
