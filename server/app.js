d5c3f89b20fba761207f715cc1262ff3b582e76a
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-11 11:27:10 -0400

    adds some associations in seeded data and removes followers model

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 // const morgan = require('morgan');
 
 // app.use(morgan('dev')); //logging middleware
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use('/api', require('./api'))
+app.use('/api', require('./api'));
 
-// handle 404s -- need to inset a Not Found Page
+// handle 404s -- need to create a Not Found Page
 app.use((req, res, next) => {
-  res.status(404).send(/* Not Found Page */)
-})
+	res.status(404).send(/* Not Found Page */);
+});
 
 // handle any errors
 app.use((err, req, res, next) => {
-  console.error(err)
-  res.status(err.status || 500).send(err.message)
-})
+	console.error(err);
+	res.status(err.status || 500).send(err.message);
+});
 
-module.exports = app
+module.exports = app;
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,35 +1,36 @@
-const db = require('../db')
-const Recipe = require('./recipe')
-const User = require('./user')
-const Comment = require('./comment')
-const Follower = require('./follower')
+const db = require('../db');
+const Recipe = require('./recipe');
+const User = require('./user');
+const Comment = require('./comment');
 
 // Model Associations
 
 // User-Recipe 1:M
-User.hasMany(Recipe)
-Recipe.belongsTo(User)
+User.hasMany(Recipe);
+Recipe.belongsTo(User);
 
 // Recipe-Comment 1:M
-Recipe.hasMany(Comment)
-Comment.belongsTo(Recipe)
+Recipe.hasMany(Comment);
+Comment.belongsTo(Recipe);
 
-// User-User (as Follower) M:M
+// User-Comment 1:M
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+// User-User (through following) M:M
 User.belongsToMany(User, {
-  as: 'follower',
-  foreignKey: 'followerId',
-  through: 'Follower',
-})
+	as: 'follower',
+	foreignKey: 'followerId',
+	through: 'followings'
+});
 User.belongsToMany(User, {
-  as: 'user',
-  foreignKey: 'userId',
-  through: 'Follower',
-})
+	as: 'user',
+	foreignKey: 'userId',
+	through: 'followings'
+});
 
 module.exports = {
-  db,
-  Recipe,
-  User,
-  Comment,
-  Follower,
-}
+	Recipe,
+	User,
+	Comment
+};
