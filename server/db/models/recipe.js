e8fe89a42cce8611f72ad1ead76fff1a2c081e26
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-13 15:52:31 -0400

    adds notEmpty validation on all fields for Recipe model

--- server/db/models/recipe.js
+++ server/db/models/recipe.js
@@ -4,18 +4,33 @@ const db = require('../db')
 const Recipe = db.define('recipe', {
   name: {
     type: Sequelize.STRING,
+    validate: {
+      notEmpty: true,
+    },
   },
   imageUrl: {
     type: Sequelize.TEXT,
+    validate: {
+      notEmpty: true,
+    },
   },
   time: {
     type: Sequelize.STRING,
+    validate: {
+      notEmpty: true,
+    },
   },
   ingredients: {
     type: Sequelize.ARRAY(Sequelize.STRING),
+    validate: {
+      notEmpty: true,
+    },
   },
   directions: {
     type: Sequelize.ARRAY(Sequelize.TEXT),
+    validate: {
+      notEmpty: true,
+    },
   },
 })
 
